dcf7ceccece64584f588753dde0de1e97360762d
lizeyang1995 <424011527@qq.com>
2020-11-16 23:27:40 +0800

    [lizeyang] feat: complete practice1

--- src/main/java/com/thoughtworks/capability/gtb/Practice1.java
+++ src/main/java/com/thoughtworks/capability/gtb/Practice1.java
@@ -1,6 +1,8 @@
 package com.thoughtworks.capability.gtb;
 
 import java.time.LocalDate;
+import java.time.Month;
+import java.time.Period;
 
 /**
  * 计算任意日期与下一个劳动节相差多少天
@@ -11,6 +13,13 @@ import java.time.LocalDate;
 public class Practice1 {
 
   public static long getDaysBetweenNextLaborDay(LocalDate date) {
-    return 0;
+    int month = date.getMonthValue();
+    int year = date.getYear();
+    if (month <= 5) {
+      LocalDate laborDay = LocalDate.of(year, 5, 1);
+      return laborDay.toEpochDay() - date.toEpochDay();
+    }
+    LocalDate laborDay = LocalDate.of(year + 1, 5, 1);
+    return laborDay.toEpochDay() - date.toEpochDay();
   }
 }
